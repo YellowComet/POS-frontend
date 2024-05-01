c6db07d38b489841abe478d68fcae2b0486e2bb1
Diego Miret <dmirethalffter@gmail.com>
2024-05-01 23:34:15 +0200

    FixBugs Categories

--- admin/src/components/partials/miniComponent/CardHeader.js
+++ admin/src/components/partials/miniComponent/CardHeader.js
@@ -5,8 +5,11 @@ const CardHeader = (props) => {
     return (
         <div className="d-flex justify-content-between align-items-center">
             <h4 className="text-theme">{props.title}</h4>
-            <button className="btn btn-primary theme-button"><Link to={props.link}>
-                <i className={`fa-solid ${props.icon}`}/> {props.button_text}</Link></button>
+                <Link to={props.link}>
+                    <button className="btn btn-primary theme-button">
+                    <i className={`fa-solid ${props.icon}`}/> {props.button_text}
+                    </button>
+                </Link>
         </div>
     );
 };
