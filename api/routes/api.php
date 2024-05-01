d57cd447037db85b12fd4b5372f76055a0d1fdfa
Diego Miret <dmirethalffter@gmail.com>
2024-05-01 23:09:05 +0200

    Update api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use \App\Http\Controllers\AuthController;
 use \App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ Route::group(['middleware'=> 'auth:sanctum'], static function () {
     Route::post('logout', [AuthController::class,'logout']);
     Route::get('get-category-list', [CategoryController::class, 'get_category_list']);
     Route::apiResource('category', CategoryController::class);
+    Route::apiResource('sub-category', SubCategoryController::class);
 });
